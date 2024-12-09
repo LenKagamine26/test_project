d2884cce1db85147352e162f34e1b96865dc8957
User <User@LEN-KAGAMINE26->
2024-12-09 10:17:39 +0300

    Добавьте файлы проекта.

---
+++ test/test.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class complex {
+private:
+    double re, im; 
+
+public:
+    complex(double r = 0, double i = 0) : re(r), im(i) {}; 
+    void display() { cout << re << ", " << im << endl; } 
+    
+    bool operator<(const complex& other) const {
+        return hypot(re, im) < hypot(other.re, other.im); // Сравниваем модули комплексных чисел
+    }
+};
+
+int main(){
+    complex a(1.2, 3.4);
+    complex b(5.6, 7.8);
+    complex c;
+    if (a < b) {
+        a.display();
+    }
+    else {
+        b.display();
+    }
+    system("pause");
+}
